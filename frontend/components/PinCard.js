018cb9e58f08ab4bd9817fcad08eefa9519b2a63
jacintogomez <63368475+jacintogomez@users.noreply.github.com>
2025-06-18 11:18:53 -0400

    eliminate webstorm auto-styling

--- frontend/components/PinCard.js
+++ frontend/components/PinCard.js
@@ -1,16 +1,16 @@
-import React, { useState } from 'react';
-import { Image } from 'react-native';
-import { Card } from 'react-native-paper';
+import React, {useState} from 'react';
+import {Image} from 'react-native';
+import {Card} from 'react-native-paper';
 
-const PinCard = ({ item, navigation, styles }) => {
-    const [imageError, setImageError] = useState(false);
+const PinCard=({item,navigation,styles}) =>{
+    const [imageError, setImageError]=useState(false);
 
     return (
-        <Card style={styles.pinCard} onPress={() => navigation.navigate('PinDetail', { pinId: item._id })}>
+        <Card style={styles.pinCard} onPress={()=>navigation.navigate('PinDetail',{ pinId:item._id })}>
             <Image
-                source={imageError || !item.imageUrl ? require('../assets/no-image.png') : { uri: item.imageUrl }}
+                source={imageError||!item.imageUrl ? require('../assets/no-image.png'):{uri:item.imageUrl }}
                 style={styles.pinImage}
-                onError={() => setImageError(true)}
+                onError={()=>setImageError(true)}
                 resizeMode="cover"
             />
             <Card.Title

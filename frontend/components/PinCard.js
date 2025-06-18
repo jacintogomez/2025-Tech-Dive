f857477b9854e75dc3f59abfd1c33d324e572c08
jacintogomez <63368475+jacintogomez@users.noreply.github.com>
2025-06-18 02:39:03 -0400

    Moved PinCard definition from HomeScreen into its own file

---
+++ frontend/components/PinCard.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import { Image } from 'react-native';
+import { Card } from 'react-native-paper';
+
+const PinCard = ({ item, navigation, styles }) => {
+    const [imageError, setImageError] = useState(false);
+
+    return (
+        <Card style={styles.pinCard} onPress={() => navigation.navigate('PinDetail', { pinId: item._id })}>
+            <Image
+                source={imageError || !item.imageUrl ? require('../assets/no-image.png') : { uri: item.imageUrl }}
+                style={styles.pinImage}
+                onError={() => setImageError(true)}
+                resizeMode="cover"
+            />
+            <Card.Title
+                title={item.title}
+                subtitle={item.description}
+                titleNumberOfLines={2}
+                subtitleNumberOfLines={2}
+                style={styles.pinTitle}
+            />
+        </Card>
+    );
+};
+
+export default PinCard;
